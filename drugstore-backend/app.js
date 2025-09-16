800a7a73abd0072d3881c0fd2dc65222510aafc2
Miguel Guerrero <m.andres655@gmail.com>
2025-09-16 02:30:38 -0500

    Update app

--- drugstore-backend/app.js
+++ drugstore-backend/app.js
@@ -5,12 +5,18 @@ const cors = require('cors')
 const productosRouter = require('./controllers/productos')
 const proveedoresRouter = require('./controllers/proveedores')
 const registrosRouter = require('./controllers/registros')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/productos', productosRouter)
 app.use('/api/proveedores', proveedoresRouter)
 app.use('/api/registros', registrosRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
