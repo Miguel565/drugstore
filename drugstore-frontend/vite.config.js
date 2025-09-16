c8ce3a92f26fe448d59422e6f4646da8d876b32c
Miguel Guerrero <m.andres655@gmail.com>
2025-09-16 10:17:27 -0500

    Update vite.config

--- drugstore-frontend/vite.config.js
+++ drugstore-frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cwd } from 'process'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-      }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        }
+      },
     },
-  },
-})
+  }
+});
